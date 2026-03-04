001bb09504ec70065f4ef8497a0a350899426def
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-03-04 11:03:41 +0500

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from .models import Student
 from .models import Register
 from .models import Note
 
 admin.site.register(Note)
 admin.site.register(Register,UserAdmin)
-admin.site.register(Student)
 # Register your models here.
