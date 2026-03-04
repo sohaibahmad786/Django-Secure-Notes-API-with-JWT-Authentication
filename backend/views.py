00e4d97ef7af8bdcac8b07baf58939016b29d1c1
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-03-04 11:03:06 +0500

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -9,8 +9,6 @@ from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.authentication import TokenAuthentication,SessionAuthentication
 from rest_framework import permissions,generics
 from rest_framework.permissions import IsAuthenticated,AllowAny
-from .models import Student
-from .serializer import Student_serilizer
 from django.conf import settings
 from datetime import datetime, timedelta
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -20,37 +18,15 @@ from .models import Note
 from .serializer import Note_serilizer
 
 
-  
-# ______________________ Student Managment API___________________
-class student_list(generics.ListCreateAPIView):
-    queryset=Student.objects.all()
-    serializer_class=Student_serilizer
-class student_detail(generics.RetrieveUpdateDestroyAPIView):
-    queryset=Student.objects.all()
-    serializer_class=Student_serilizer
-    
 
-# _____________________ Banking Account API __________
-# class account_list(generics.ListCreateAPIView):
-#     queryset=Account.objects.all()
-#     serializer_class=Account_serilizer
-#     authentication_classes=[JWTAuthentication]
-#     permission_classes=[permissions.AllowAny]
-
-# class account_detail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset=Account.objects.all()
-#     serializer_class=Account_serilizer
-#     authentication_classes=[JWTAuthentication]
-#     permission_classes=[permissions.AllowAny]
-
-
-# ___________________ register________
+# ___________________ Register view________
 
 class Register_list(ListCreateAPIView):
     queryset=Register.objects.all()
     serializer_class=Register_serilizer
     permission_classes=[permissions.AllowAny]
 
+# ___________________ Note view________
 
 class Note_list(generics.ListCreateAPIView):
 
