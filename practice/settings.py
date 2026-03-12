268120ece8138e51b2d8866fe0fa510a401dc326
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-03-12 15:44:38 +0500

    Update settings.py

--- practice/settings.py
+++ practice/settings.py
@@ -78,12 +78,17 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'My_data',
+        'USER':'postgres',
+        'PASSWORD':'423168',
+        'HOST':'localhost',
+        'POST':'5432'
     }
 }
+
 
 
 # Password validation
